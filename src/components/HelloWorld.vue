1c906ae689c1f36a373d52d2044f7e222552e8b1
SPRITELEMON\Spritelemon <spritelemon36@hotmail.com>
2018-05-03 23:55:40 +0700

    font ไทย

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,9 +5,23 @@
 </template>
 
 <script>
-import pdfMake from "pdfmake/build/pdfmake"
-import pdfFonts from "pdfmake/build/vfs_fonts"
+import pdfMake from 'pdfmake/build/pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
 pdfMake.vfs = pdfFonts.pdfMake.vfs
+pdfMake.fonts = {
+  THSarabunNew: {
+    normal: 'THSarabunNew.ttf',
+    bold: 'THSarabunNew-Bold.ttf',
+    italics: 'THSarabunNew-Italic.ttf',
+    bolditalics: 'THSarabunNew-BoldItalic.ttf'
+  },
+  Roboto: {
+    normal: 'Roboto-Regular.ttf',
+    bold: 'Roboto-Medium.ttf',
+    italics: 'Roboto-Italic.ttf',
+    bolditalics: 'Roboto-MediumItalic.ttf'
+  }
+}
 export default {
   name: 'HelloWorld',
   data () {
@@ -19,25 +33,106 @@ export default {
     createPDF () {
       // var win = window.open()
       // win.document.open()
+      /* var docDefinition2 = {
+        content: [
+          {
+            style: 'tableExample',
+            table: {
+              heights: 60,
+              // widths: [ '*', 'auto', 100, '*' ],
+              body: [
+                [{text: 'Header 1', style: 'tableHeader'}, {text: 'Header 2', style: 'tableHeader'}, {text: 'Header 3', style: 'tableHeader'}, {text: 'Header 4', style: 'tableHeader'}],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+              ]
+            }
+          }
+        ],
+        styles: {
+          tableExample: {
+          margin: [0, 5, 0, 15]
+          },
+            tableHeader: {
+            bold: true,
+            fontSize: 14,
+            color: 'black'
+          }
+        }
+      } */
       var docDefinition = {
-  content: [
-    {
-      layout: 'lightHorizontalLines', // optional
-      table: {
-        // headers are automatically repeated if the table spans over multiple pages
-        // you can declare how many rows should be treated as headers
-        headerRows: 1,
-        widths: [ '*', 'auto', 100, '*' ],
-
-        body: [
-          [ 'First', 'Second', 'Third', 'The last one' ],
-          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
-          [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4' ]
-        ]
+        footer: function (currentPage, pageCount) { return currentPage.toString() + ' of ' + pageCount },
+        header: function (currentPage, pageCount, pageSize) {
+          // you can apply any logic and return any valid pdfmake element
+          return [
+            { text: 'simple text', alignment: (currentPage % 2) ? 'left' : 'right' },
+            { canvas: [ { type: 'rect', x: 170, y: 32, w: pageSize.width - 170, h: 40 } ] }
+          ]
+        },
+        content: [
+          {
+            style: 'tableExample',
+            table: {
+              heights: 60,
+              // widths: [ '*', 'auto', 100, '*' ],
+              body: [
+                [{text: 'Header 1', style: 'tableHeader'}, {text: 'Header 2', style: 'tableHeader'}, {text: 'Header 3', style: 'tableHeader'}, {text: 'Header 4', style: 'tableHeader'}],
+                [ 'สวัสดดี', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ]
+              ]
+            }
+          }
+        ],
+        defaultStyle:{
+          font: 'THSarabunNew'
+        },
+        styles: {
+          tableExample: {
+            margin: [5, 5, 0, 15],
+            fontSize: 14
+          },
+          tableHeader: {
+            bold: true,
+            fontSize: 16,
+            color: 'black'
+          }
+        }
       }
-    }
-  ]
-}
       const pdfDocGenerator = pdfMake.createPdf(docDefinition)
       pdfDocGenerator.getBase64((buffer) => {
         var iframe = "<iframe width='100%' height='100%' src='data:application/pdf;base64," + buffer + "'></iframe>" // eslint-disable-line
